3b45605959f4c2dbd7862edc534b52845e0bb938
Rabia Nur Kavi <kavirabiaa@gmail.com>
2021-05-09 21:07:36 +0300

    6.Day

---
+++ hrms/src/main/java/kodlama/hrms/business/abstracts/JobPositionService.java
@@ -0,0 +1,11 @@
+package kodlama.hrms.business.abstracts;
+
+import java.util.List;
+import kodlama.hrms.entities.concretes.JobPosition;
+
+
+public interface JobPositionService {
+
+	List<JobPosition> getAll();
+	
+}
